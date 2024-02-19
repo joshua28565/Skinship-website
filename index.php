672b570a3abdb83a202bc1d88e1f83d4e4f3fddc
Joshua Sebastian <jova.sebastian.up@phinmaed.com>
2024-02-19 23:47:07 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,436 @@
+﻿<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>SKINSHIP</title>
+	 <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon" />
+
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="assets/css/flexslider.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />    
+ 
+</head>
+<body >
+   
+ <div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img class="logo-custom" src="assets/img/logoz.png" alt=""  /></a>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li ><a href="#home">HOME</a></li>
+                     <li><a href="#testimonials-sec">RECOMMENDATION</a></li>
+                   
+                     <li><a href="#brand-sec">ABOUT US</a></li>
+					  <li><a href="#course-sec">CONTACT US</a></li>
+                     
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+     
+       <div class="home-sec" id="home" >
+           <div class="overlay">
+ <div class="container">
+           <div class="row text-center " >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+                <div class="flexslider set-flexi" id="main-section" >
+                    <ul class="slides move-me">
+                        <!-- Slider 01 -->
+                        <li>
+                              <h3>Effective Skincare Products</h3>
+                           <h1>WHAT ARE YOU WAITING FOR? SHOP NOW!</h1>
+                            <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 01 -->
+                        
+                        <!-- Slider 02 -->
+                        <li>
+                            <h3> Glow and Smooth Skin Products</h3>
+                           <h1>WE HAVE WHAT YOU NEED</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                               SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 02 -->
+                        
+                        <!-- Slider 03 -->
+                        <li>
+                            <h3>Effective Skincare Products</h3>
+                           <h1>WE HAVE ALL PRODUCTS</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 03 -->
+                    </ul>
+                </div>
+                   
+     
+              
+              
+            </div>
+                
+               </div>
+                </div>
+           </div>
+           
+       </div>
+       <!--HOME SECTION END-->   
+    <div  class="tag-line" >
+         <div class="container">
+           <div class="row  text-center" >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+        <h2 data-scroll-reveal="enter from the bottom after 0.1s" > SKINSHIP </h2>
+                   </div>
+               </div>
+             </div>
+        
+    </div>
+    <!--HOME SECTION TAG LINE END-->   
+         <div id="testimonials-sec" class="container set-pad" >
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">RECOMMENDATION </h1>
+                     
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+
+           <div class="row" >
+           
+               
+                 <div class="col-lg-3  col-md-3 col-sm-3" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="about-div">
+                  <center>     <img src="assets/img/masck.png"  class="img-rounded" style="width:100px;height:100px;"/>
+                   <h2>BIOAQUA</h2>
+                       <hr />
+                   <p >
+                   Collagen Facial Mask is an anti-aging powerhouse, specifically formulated to tackle signs of aging such as fine lines and skin laxity. Its collagen-rich formula deeply hydrates and firms the skin, revealing a more youthful and vibrant complexion.
+                   </p>
+				   </center>
+            
+                </div>
+                   </div>
+                   
+				   
+                   <div class="col-lg-3  col-md-3 col-sm-3" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="about-div">
+                  <center>     <img src="assets/img/master.png"  class="img-rounded" style="width:100px;height:100px;"/>
+                   <h2>MASTER OIL</h2>
+                       <hr />
+                   <p >
+                   Master Anti-Pimple Deep Cleanser has cooling ZEROil to control oil for up to 8 hours, antibacterial agents to help prevent blackheads/whiteheads, and dermaclear formula to help prevent pimple breakouts.
+                   </p>
+				   </center>
+            
+                </div>
+                   </div>
+				   
+				   
+                   <div class="col-lg-3  col-md-3 col-sm-3" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="about-div">
+                  <center>     <img src="assets/img/salicylic.png"  class="img-rounded" style="width:100px;height:100px;"/>
+                   <h2>SKINEVER</h2>
+                       <hr />
+                   <p >
+                   Salicylic Refreshing Scrub Cleanser can deeply cleansing the skin, its unique natural exfoliating particles can gently remove dead skin cells and clean pores, leaving the skin smoother.
+                   </p>
+				   </center>
+            
+                </div>
+                   </div>
+                 
+                   <div class="col-lg-3  col-md-3 col-sm-3" data-scroll-reveal="enter from the bottom after 0.4s">
+                     <div class="about-div">
+                  <center>     <img src="assets/img/cetaphil.png"  class="img-rounded" style="width:100px;height:100px;"/>
+                   <h2>CETHAPHIL</h2>
+                       <hr />
+                   <p >
+                   This gentle cleansing formula, now with hydrating Glycerin, Vitamin B3 & Pro-Vitamin B5, is clinically tested to be gentle at removing dirt, makeup and impurities, while preserving skin's natural moisture barrier.
+                   </p>
+				   </center>
+            
+                </div>
+                   </div>
+                 
+                 
+               </div>
+             </div>
+   <!-- FEATURES SECTION END-->
+
+		
+		<div id="brand-sec" class="container set-pad" >
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.2s"  class="header-line">ABOUT US</h1>
+                     
+               
+						 
+					 
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+<div class="container">
+           <div class="row" >
+           
+               
+                 
+                    <img class="img img-rounded img-responsive" src="assets/img/aboutus.png"  style="width:3000px;height:560px;"/>
+				   
+				  
+                 
+				 <div class="col-lg-12">
+				 
+                    
+                   </div>
+                 
+                 </div>
+               </div>
+             </div>
+			 <br />
+    <!-- FACULTY SECTION END-->
+      <div id="course-sec" class="container set-pad">
+             <div class="row text-center">
+                 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                     <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">CONTACT US</h1>
+                     <p data-scroll-reveal="enter from the bottom after 0.3s">
+                      If you have any questions, please feel free to contact us, our customer service center is working for you 24/7.
+					  <br />For more details. See the contact information below.
+                         </p>
+                 </div>
+
+             </div>
+             <!--/.HEADER LINE END-->
+
+<br />
+           
+             <div class="container">
+             <div class="row set-row-pad"  >
+    <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1 " data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong>Our Location </strong></h2>
+        <hr />
+                    <div ">
+                        <h4><strong>Location:</strong>123 Burgos Street, Mangatarem ,</h4>
+                        <h4>Pangasinan,Philippines</h4>
+				
+                        
+                    </div>
+
+
+                </div>
+                 <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1" data-scroll-reveal="enter from the bottom after 0.4s">
+
+                    <h2 ><strong>Feedbacks </strong></h2>
+        <hr />
+                    <div >
+                        <h4><strong>Call:</strong>  +63 555 444 333 </h4>
+                        <h4><strong>Email: </strong>Skinship@gmail.com</h4>
+                    </div>
+                    </div>
+
+
+                </div>
+                 </div>
+                 
+                 
+               </div>
+             </div>
+      <!-- COURSES SECTION END-->
+     <div class="modal fade" id="su" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Registration Form</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="register.php">
+                   <fieldset>
+					
+							<div class="form-group">
+                                <input class="form-control" placeholder="Firstname" name="ruser_firstname" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Lastname" name="ruser_lastname" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Address" name="ruser_address" type="text" required>
+							</div>
+							
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Email" name="ruser_email" type="email" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="ruser_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="register">Sign Up</button>
+				
+				 <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+              </div>
+            </div>
+          </div>
+        </div>
+<!-- Script -->
+
+
+     <div class="modal fade" id="ln" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Customer Login</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="userlogin.php">
+                   <fieldset>
+					
+						
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Email" name="user_email" type="email" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="user_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="user_login">Sign In</button>
+				
+				 <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+				   
+              </div>
+            </div>
+          </div>
+        </div>
+		
+		<div class="modal fade" id="an" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+           <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Administrator Credentials</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="adminlogin.php">
+                   <fieldset>
+					
+						
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Username" name="admin_username" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="admin_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-warning btn-block" name="admin_login">Login</button>
+				
+				 <button type="button" class="btn btn-md btn-success btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+              </div>
+            </div>
+          </div>
+        </div>
+		 <br />
+			 <br />
+			 <br />
+<!-- Script -->
+     <!-- CONTACT SECTION END-->
+    <div id="footer">
+    <div class="alert alert-default" style="background-color:#033c73;">
+                       <p style="color:white;text-align:center;">
+                       &copy 2024 SKINSHIP| All Rights Reserved |  Design by : DEVKNIGHT Team
+    </div>
+     <!-- FOOTER SECTION END-->
+   
+    <!--  Jquery Core Script -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!--  Core Bootstrap Script -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!--  Flexslider Scripts --> 
+         <script src="assets/js/jquery.flexslider.js"></script>
+     <!--  Scrolling Reveal Script -->
+    <script src="assets/js/scrollReveal.js"></script>
+    <!--  Scroll Scripts --> 
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!--  Custom Scripts --> 
+         <script src="assets/js/custom.js"></script>
+</body>
+</html>
